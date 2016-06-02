7afdb32629b445f3ec9d88143be271b9a736d011
GavinChiang <cchsuan@umich.edu>
2016-06-03 02:59:41 +0800

    user edit page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,6 +30,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [320, 150]
   end
 
+  version :slidepage do
+    process resize_to_fit: [800, 300]
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
