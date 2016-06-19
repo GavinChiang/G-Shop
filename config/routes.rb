6107cd6b8eb6123b43197c8b40eaeaa419a35784
GavinChiang <cchsuan@umich.edu>
2016-06-19 21:06:08 +0800

    add about me page

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,7 @@ Rails.application.routes.draw do
     end
 
     resources :items, controller: "cart_items"
+    get "/pages/:action" , :controller => "pages"
     # The priority is based upon order of creation: first created -> highest priority.
       # See how all your routes lay out with "rake routes".
 
